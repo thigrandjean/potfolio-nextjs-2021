9f273349dbbe2212e17b71c496e470dfc5d6f646
Thiago Grandjean <thigrandjean@yahoo.com.br>
2021-03-08 22:00:56 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,20 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>Boilerplate React</S.Title>
+    <S.Description>
+      TypeSccript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvedor de frente para o código"
+    />
+  </S.Wrapper>
+)
+
+export default Main
